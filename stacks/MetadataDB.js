0d1087aed70798835f86a6ef8e0ee85bb6ad094a
Yessica Hernandez <yessicahernandez@Yessicas-MacBook-Pro.local>
2022-12-12 03:58:47 -0600

    Enabling CORS

--- stacks/MetadataDB.js
+++ stacks/MetadataDB.js
@@ -2,7 +2,16 @@ import { Bucket, Table } from "@serverless-stack/resources";
 
 export function MetadataDB({ stack, app }) {
     // Create an S3 bucket
-    const bucket = new Bucket(stack, "logfiles");
+    const bucket = new Bucket(stack, "logfiles", {
+        cors: [
+          {
+            maxAge: "1 day",
+            allowedOrigins: ["*"],
+            allowedHeaders: ["*"],
+            allowedMethods: ["GET", "PUT", "POST", "DELETE", "HEAD"],
+          },
+        ],
+      });
   // Create the DynamoDB table
     const table = new Table(stack, "MetadataDictionary", {
     fields: {
