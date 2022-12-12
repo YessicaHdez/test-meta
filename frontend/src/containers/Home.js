6e766e13ea4adf502d70c7c68c4d8f24e520f3f2
Yessica Hernandez <yessicahernandez@Yessicas-MacBook-Pro.local>
2022-12-12 04:33:40 -0600

    Setting up our React app

---
+++ frontend/src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>AAAAAAAXS</h1>
+        <p className="text-muted"></p>
+      </div>
+    </div>
+  );
+}
